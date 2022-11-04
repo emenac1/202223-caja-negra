72fa9ee446b3769e47b197637392be8d6a491686
emenac <e.menac@alumnos.upm.es>
2022-11-04 21:17:15 +0100

    Estructura Test Creada

---
+++ src/test/java/com/practica/cajanegra/test.java
@@ -0,0 +1,59 @@
+package com.practica.cajanegra;
+
+import com.binarytree.*;
+
+import static org.junit.Assert.*;
+
+public class test {
+
+    @org.junit.jupiter.api.Test
+    void size() {
+        BinaryTree bt = new BinaryTree<String>("2");
+        assertEquals(1, bt.size());
+    }
+
+    @org.junit.jupiter.api.Test
+    void depth() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testDepth() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRoot() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void insert() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void remove() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void search() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testEquals() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void toList() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSubTree() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void iterator() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+
+}
